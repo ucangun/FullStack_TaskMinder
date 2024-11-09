970698a9c061e2f99ebb2fb6a44bf01fcd311b3d
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-09 14:36:18 +0100

    "Added create, edit, and delete todo functionality to useTodoCall hook"

--- src/hooks/useTodoCall.jsx
+++ src/hooks/useTodoCall.jsx
@@ -3,16 +3,50 @@ import axios from "axios";
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
 const useTodoCall = () => {
+  // Get all todos
   const getAllTodos = async () => {
     try {
-      const { data } = await axios.get(`${baseUrl}todo`);
+      const { data } = await axios.get(`${baseUrl}/todo`);
       console.log(data);
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching todos:", error);
     }
   };
 
-  return { getAllTodos };
+  // Create a new todo
+  const createNewTodo = async (newTodo) => {
+    try {
+      const { data } = await axios.post(`${baseUrl}/todo`, newTodo);
+      console.log(data);
+    } catch (error) {
+      console.error("Error creating todo:", error);
+    }
+  };
+
+  // Edit an existing todo
+  const editTodo = async (todoId, updatedTodo) => {
+    try {
+      const { data } = await axios.put(
+        `${baseUrl}/todo/${todoId}`,
+        updatedTodo
+      );
+      console.log(data);
+    } catch (error) {
+      console.error("Error editing todo:", error);
+    }
+  };
+
+  // Delete a todo
+  const deleteTodo = async (todoId) => {
+    try {
+      const { data } = await axios.delete(`${baseUrl}/todo/${todoId}`);
+      console.log(data);
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
+  };
+
+  return { getAllTodos, createNewTodo, editTodo, deleteTodo };
 };
 
 export default useTodoCall;
